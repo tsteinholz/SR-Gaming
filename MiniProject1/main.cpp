0b3b54b50097197766fd6765e28ea1b0258cb29f
Thomas Steinholz <tsteinholz@yahoo.com>
2015-10-05 08:44:37 -0400

    Allegro fun

--- MiniProject1/main.cpp
+++ MiniProject1/main.cpp
@@ -1,8 +1,39 @@
-//#include <allegro.h>
+#include <allegro.h>
+
+void init();
+void deinit();
 
 int main() {
-//    allegro_init();
-//    allegro_message("Hello World!");
-    return 0;
+	init();
+
+	while (!key[KEY_ESC]) {
+		/* put your code here */
+	}
+
+	deinit();
+	return 0;
 }
-//END_OF_MAIN()
+END_OF_MAIN()
+
+void init() {
+	int depth, res;
+	allegro_init();
+	depth = desktop_color_depth();
+	if (depth == 0) depth = 32;
+	set_color_depth(depth);
+	res = set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
+	if (res != 0) {
+		allegro_message(allegro_error);
+		exit(-1);
+	}
+
+	install_timer();
+	install_keyboard();
+	install_mouse();
+	/* add other initializations here */
+}
+
+void deinit() {
+	clear_keybuf();
+	/* add other deinitializations here */
+}
