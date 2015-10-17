eef8845dec06a1fbf863277f0af19a1a497159f3
Thomas Steinholz <tsteinholz@yahoo.com>
2015-10-17 13:57:47 -0400

    finished miniproject1

--- Projects/MiniProject1/main.cpp
+++ Projects/MiniProject1/main.cpp
@@ -9,46 +9,52 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 
-int main(){
-    printf("We started.\n");
-
+int main()
+{
     ALLEGRO_DISPLAY *display = NULL;
+    ALLEGRO_FONT* century_gothic24;
 
-    //Initializing Allegro and see if it is allright.
-    if(!al_init()) {
+    if(!al_init())
+    {
         printf("al_init Failed!\n");
         return -1;
     }
-
-    //Initializing the primitives addon.
-    if(!al_init_primitives_addon()) {
+    if(!al_init_primitives_addon())
+    {
         printf("al_init_primitives_addon Failed!\n");
         return -1;
     }
-
-    //Create the display (window).
     display = al_create_display(640, 480);
-
-    //Check if the window was created Successfully.
-    if(!display) {
+    if(!display)
+    {
         printf("al_create_display Failed!\n");
         return -1;
     }
-
-    //Paint, paint, paint, Paint it black!
-    //Actually, paind a hidden screen (a buffer), that we'll apply later.
+    al_init_font_addon();
+    al_init_ttf_addon();
+    century_gothic24  = al_load_ttf_font("C:\\Windows\\Fonts\\GOTHIC.TTF" , 24, ALLEGRO_ALIGN_CENTRE);
+    // Paint a hidden screen (a buffer), that we'll apply later.
     al_clear_to_color(al_map_rgb(0,0,0));
-
-    //Draw an ugly red triangle to the buffer.
+    // Draw an ugly red triangle to the buffer.
     al_draw_filled_triangle(10, 10, 200, 200, 350, 230, al_map_rgb(255,0,0));
-
-    //Apply the buffer to the front.
+    // Draw text to the buffer
+    al_draw_text(century_gothic24, al_map_rgb(255,255,255), 550, 440, ALLEGRO_ALIGN_CENTRE, "Hello World!");
+    // Draw a line to the buffer
+    al_draw_line(400,400,500,34,al_map_rgb(255,255,255), 5);
+    // Draw a circle
+    al_draw_circle(300,300, 50, al_map_rgb(255,255,0), 5);
+    // Draw Eclipse
+    al_draw_filled_ellipse(500, 300, 40, 56, al_map_rgb(255,0,255));
+    // Draw a rectangle
+    al_draw_rectangle(100,200,200,300,al_map_rgb(0,255,0), 6);
+    // Draw a polygon
+    al_draw_filled_rectangle(0,480,50,400,al_map_rgb(25,185,35));
+    al_draw_filled_rectangle(0,480,200,450,al_map_rgb(25,185,35));
+    // Apply the buffer to the front.
     al_flip_display();
-
-    //Wait for 3 secs.
+    // Wait for 3 secs.
     al_rest(3.0);
-
-    //Destroying the display we created, to avoid memory leaking.
+    // Destroying the display we created, to avoid memory leaking.
     al_destroy_display(display);
 
     return 0;
