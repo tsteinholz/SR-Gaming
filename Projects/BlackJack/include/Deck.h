4755dd77eeb9fca77b0456c5e4d8f7e9ec3ff365
Thomas Steinholz <tsteinholz@yahoo.com>
2015-11-09 08:43:14 -0500

    stuff

--- Projects/BlackJack/include/Deck.h
+++ Projects/BlackJack/include/Deck.h
@@ -1,12 +1,18 @@
 #ifndef BLACK_JACK_DECK_H
 #define BLACK_JACK_DECK_H
 
+#include <vector>
+
+#include "Cards.h"
 
 class Deck
 {
     public:
         Deck();
         virtual ~Deck();
+
+        typedef std::vector<Cards> Cards;
+
     protected:
     private:
 };
