9eada61c8ece3edbf982a7f761dbb4c4ca14e1c4
Thomas Steinholz <tsteinholz@yahoo.com>
2015-11-11 21:20:47 -0500

    yup

--- Projects/BlackJack/src/main.cpp
+++ Projects/BlackJack/src/main.cpp
@@ -6,8 +6,34 @@ int main() {
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::Resized:break;
+                case sf::Event::LostFocus:break;
+                case sf::Event::GainedFocus:break;
+                case sf::Event::TextEntered:break;
+                case sf::Event::KeyPressed:break;
+                case sf::Event::KeyReleased:break;
+                case sf::Event::MouseWheelMoved:break;
+                case sf::Event::MouseWheelScrolled:break;
+                case sf::Event::MouseButtonPressed:break;
+                case sf::Event::MouseButtonReleased:break;
+                case sf::Event::MouseMoved:break;
+                case sf::Event::MouseEntered:break;
+                case sf::Event::MouseLeft:break;
+                case sf::Event::JoystickButtonPressed:break;
+                case sf::Event::JoystickButtonReleased:break;
+                case sf::Event::JoystickMoved:break;
+                case sf::Event::JoystickConnected:break;
+                case sf::Event::JoystickDisconnected:break;
+                case sf::Event::TouchBegan:break;
+                case sf::Event::TouchMoved:break;
+                case sf::Event::TouchEnded:break;
+                case sf::Event::SensorChanged:break;
+                case sf::Event::Count:break;
+            }
         }
     }
 
